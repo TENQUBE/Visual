43570c4bc36af8d80924c25470ad5b66627d36e2
tenqube <kj.sa@tenqube.com>
2022-07-13 15:40:37 +0900

    0.4.5

--- Package.swift
+++ Package.swift
@@ -20,7 +20,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Visual",
-            dependencies: []),
+            dependencies: [
+                .package(url: "https://github.com/Alamofire/Alamofire.git", .branch("master"))
+            ]),
         .testTarget(
             name: "VisualTests",
             dependencies: ["Visual"]),
